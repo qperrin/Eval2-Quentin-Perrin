20620a376599ee565f08471444365de1058411fc
Quentin Perrin <qperrin382@gmail.com>
2021-03-22 14:16:22 +0100

    Logique de base Game

---
+++ src/Game.js
@@ -0,0 +1,11 @@
+export class Game {
+  constructor(players) {
+    this.players = players;
+    this.turn = 1;
+    this.winner = null;
+  }
+
+  checkWinner() {
+    this.players.forEach((player) => {});
+  }
+}
